11597108364652fe6a004899972261a4eb8625c2
ERJ97 <jones.elliot1000@gmail.com>
2026-01-29 15:58:16 +0000

    Initial Commit

---
+++ Lecture1.py
@@ -0,0 +1,49 @@
+# test class 
+# Python automatically decides the data type for you. No need to decalre it. 
+x1 = 2 + 3* 4
+print(x1, type (x1))
+
+x2 = 2 ** 10
+print(x2, type(x2))
+
+x3 = 6/3
+print(x3, type(x3))
+
+x4 = 7/4 
+print(x4, type(x4))
+
+x5 = 7 // 3
+print(x5, type (x5))
+
+x6 = 7 % 3 
+print(x6, type (x6))
+
+x7 = 3 / 6 
+print (x7, type (x7))
+
+# Boolean value check 
+
+print( 5 == 10 )
+print ( 10 > 5)
+print (( 5 >= 1 ) and ( 5 <= 10 ))
+
+# You can manipulate varibales and override them: 
+
+theSum = 0
+print(theSum, type(theSum))
+
+theSum = True 
+print(theSum, type(theSum))
+
+# Lists - comma-delimited, enclosed in square brackets []
+
+myList = [1,3,True,6.5]
+print(myList)
+
+
+# Operators can be used on any sequence in Python: 
+
+myList = [1,2,3,4]
+A = [myList] * 3
+print(A)
+
